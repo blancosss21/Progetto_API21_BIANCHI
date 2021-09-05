f852cfa895e8ecd0b313eb2cee8495831b8e8fff
994565 <matteo21.bianchi@mail.polimi.it>
2021-09-05 15:08:03 +0200

    progetto ma senza ottimizzazioni

--- main.c
+++ main.c
@@ -6,104 +6,112 @@
 
 #define MAX 30000
 
-unsigned int **Matrice;
+int **Matrice;
 
 struct prestazioneGrafo {
-    unsigned int numeroGrafo;
-    unsigned int costoGrafo;
+    int numeroGrafo;
+    int costoGrafo;
 };
 
-unsigned int minDistance(unsigned int dist[], bool sptSet[], unsigned int d) {
-    // Initialize min value
-    unsigned int min = INT_MAX, min_index =0;
-
-    for (unsigned int v = 0; v < d; v++)
+int minDistance(int dist[], bool sptSet[], int d) {
+    int min = INT_MAX, min_index = 0;
+    for (int v = 0; v < d; v++)
         if (sptSet[v] == false && dist[v] <= min)
             min = dist[v], min_index = v;
-
     return min_index;
 }
 
-unsigned int myAtoi(char *str) {
-    unsigned int res = 0;
-    for (unsigned int i = 0; str[i] != '\0' && str[i] != '\n'; ++i)
+int myAtoi(char *str) {
+    int res = 0;
+    for (int i = 0; str[i] != '\0' && str[i] != '\n'; ++i)
         res = res * 10 + str[i] - '0';
     return res;
 }
 
-unsigned int dijkstra(unsigned int d) {
-    unsigned int dist[d];
+int dijkstra(int d) {
+    int dist[d];
     bool sptSet[d];
-    unsigned int somma = 0;
-    for (unsigned int i = 0; i < d; i++)
+    int somma = 0;
+    for (int i = 0; i < d; i++)
         dist[i] = INT_MAX, sptSet[i] = false;
     dist[0] = 0;
-    for (unsigned int count = 0; count < d - 1; count++) {
-        unsigned int u = minDistance(dist, sptSet, d);
+    for (int count = 0; count < d - 1; count++) {
+        int u = minDistance(dist, sptSet, d);
         sptSet[u] = true;
-        for (unsigned int v = 0; v < d; v++)
+        for (int v = 0; v <= d; v++)
             if (!sptSet[v] && Matrice[u][v] && dist[u] != INT_MAX
-                && dist[u] + Matrice[u][v] < dist[v])
-                dist[v] = dist[u] + Matrice[u][v];
+                && dist[u] + Matrice[u][v] < dist[v]) {
+                dist[v] = Matrice[u][v] + dist[u];
+            }
     }
-    for (unsigned int i = 0; i < d; i++)
-        somma = somma + dist[i];
+    for (int i = 0; i < d; i++) {
+        if (dist[i] < INT_MAX) {
+            somma = somma + dist[i];
+        }
+    }
     return somma;
 }
 
-unsigned int AggiungiGrafo(unsigned int d) {
+void aggiungiGrafo(int d) {
     char riga[MAX];
     char *tok;
-    unsigned int c;
-    for (c = 0; c < d; c++) {
+    for (int c = 0; c < d; c++) {
         if (fgets(riga, MAX, stdin) == NULL) {
             exit(1);
         }
         tok = strtok(riga, ",");
         Matrice[c][0] = myAtoi(tok);
-        for (unsigned int e = 1; e < d; e++) {
+        for (int e = 1; e < d; e++) {
             tok = strtok(NULL, ",");
             Matrice[c][e] = myAtoi(tok);
         }
     }
-    return dijkstra(d);
 }
 
 
-void aggiornaCamminiMinimi(struct prestazioneGrafo *camminiMinimi, unsigned int k, unsigned int nuovoCammino, unsigned int numGrafo) {
-    unsigned int i;
-    for (i = 0; i < k; i++) {
-        if(camminiMinimi[i].numeroGrafo == 0){
+void aggiornaCamminiMinimi(struct prestazioneGrafo *camminiMinimi, int k, int nuovoCammino, int numGrafo) {
+    for (int i = 0; i < k; i++) {
+        if (camminiMinimi[i].numeroGrafo == -1) {
             camminiMinimi[i].numeroGrafo = numGrafo;
             camminiMinimi[i].costoGrafo = nuovoCammino;
             return;
         }
     }
-    unsigned int posMax = 0;
-    unsigned int max = camminiMinimi[0].costoGrafo;
-    for (i = 0; i < k; i++) {
-        if(camminiMinimi[i].costoGrafo > max){
+    int posMax = 0;
+    int max = camminiMinimi[0].costoGrafo;
+    for (int i = 0; i < k; i++) {
+        if (camminiMinimi[i].costoGrafo > max) {
             max = camminiMinimi[i].costoGrafo;
             posMax = i;
         }
     }
-    if (nuovoCammino < max){
+    if (nuovoCammino < max) {
         camminiMinimi[posMax].costoGrafo = nuovoCammino;
         camminiMinimi[posMax].numeroGrafo = numGrafo;
     }
-    return;
+}
+
+/*void topkCompleto(struct prestazioneGrafo *camminiMinimi, int k) {
+    for (int i = 0; i < k; ++i) {
+        if (camminiMinimi[i].numeroGrafo != -1)
+            printf("il grafo %d ha un percorso di %d\n", camminiMinimi[i].numeroGrafo, camminiMinimi[i].costoGrafo);
+    }
+}*/
+
+void topk(struct prestazioneGrafo *camminiMinimi, int k) {
+
+    for (int i = 0; i < k; ++i) {
+        printf("%d ", camminiMinimi[i].numeroGrafo);
+
+
+    }
 }
 
 
 int main() {
-    unsigned int d;
-    unsigned int k;
-
-    unsigned int i;
-    unsigned int nuovoCammino;
-    unsigned int numGrafo = 1;
-
-
+    int d;
+    int k;
+    int numGrafo = 0;
     if (scanf("%u %u\n", &d, &k) != 2) {
         return -1;
     }
@@ -111,31 +119,26 @@ int main() {
     if (d == 0) {
         return -1;
     }
-    Matrice = (unsigned int **) malloc(d * sizeof(unsigned int *));
-    for (i = 0; i < d; i++) {
-        Matrice[i] = (unsigned int *) malloc(d * sizeof(unsigned int));
+    Matrice = (int **) malloc(d * sizeof(int *));
+    for (int i = 0; i < d; i++) {
+        Matrice[i] = (int *) malloc(d * sizeof(int));
     }
-
-    for (i = 0; i < k; i++) {
-        camminiMinimi[i].numeroGrafo = 0;
-        camminiMinimi[i].numeroGrafo = 0;
+    for (int i = 0; i < k; i++) {
+        camminiMinimi[i].numeroGrafo = -1;
+        camminiMinimi[i].costoGrafo = -1;
     }
     char str1[] = "AggiungiGrafo";
     char str2[] = "TopK";
     char *res = malloc(14 * sizeof(char));  //prima per 13
-    while (1) {
+    while (!feof(stdin)) {
         if (scanf("%s\n", res) != 1) {
             return -1;
+        } else if (!strcmp(res, str1)) {
+            aggiungiGrafo(d);
+            aggiornaCamminiMinimi(camminiMinimi, k, dijkstra(d), numGrafo);
+        } else if (!strcmp(res, str2)) {
+            topk(camminiMinimi, k);
         }
-        if (feof(stdin))
-            break;
-        if (!strcmp(res, str1)) {
-            nuovoCammino = AggiungiGrafo(d);
-            aggiornaCamminiMinimi(camminiMinimi, k, nuovoCammino, numGrafo);
-        } else if (!strcmp(res, str2))
-            printf("ciao");
-        else
-            break;
         numGrafo++;
     }
     return 0;
